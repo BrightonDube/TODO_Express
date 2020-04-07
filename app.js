ae93ad956f04c51145fcb4d13fc31d73d83c56ce
BrightonDube <BrightonDube@users.noreply.github.com>
2020-04-07 06:52:14 +0200

    refactor

--- app.js
+++ app.js
@@ -1,44 +1,23 @@
 //jshint esversion:6
-
 const express = require('express');
-const https = require("https");
 const app = express();
 const port = 3200;
+const date = require(__dirname+'/date.js');
+
 
 // view engine setup
 app.set('views', (__dirname+'/views'));
-
 app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
-
+app.use(express.static(__dirname+'/public'));
 const items = [];
 
-
-
-app.use(express.static(__dirname+'/public'));
-
 app.get('/', (req, res) => {
-
-  //res.render('index', {title: "Home"});
-  const today = new Date();
-  
-  // const weekday = new Array(7);
-  // weekday[0] = "Sunday";
-  // weekday[1] = "Monday";
-  // weekday[2] = "Tuesday";
-  // weekday[3] = "Wednesday";
-  // weekday[4] = "Thursday";
-  // weekday[5] = "Friday";
-  // weekday[6] = "Saturday";
-  
-  //const day = weekday[today.getDay()]; 
-  //res.render("index",{ day : day});
-  const options = {weekday:"long",day: "numeric", month: "long", year: "numeric"};// options to pass to the toLocale method
-  const day = today.toLocaleString("us-En", options);
+  let day = date.day;
   res.render('index', {title: day, day: day, items: items});
 });
 
---
+++ date.js
@@ -0,0 +1,26 @@
+//jshint esversion: 6
+module.exports = new GetDay();
+
+function GetDay(){
+    //res.render('index', {title: "Home"});
+const today = new Date();
+  
+// const weekday = new Array(7);
+// weekday[0] = "Sunday";
+// weekday[1] = "Monday";
+// weekday[2] = "Tuesday";
+// weekday[3] = "Wednesday";
+// weekday[4] = "Thursday";
+// weekday[5] = "Friday";
+// weekday[6] = "Saturday";
+
+//const day = weekday[today.getDay()]; 
+//res.render("index",{ day : day});
+options = {
+    weekday:"long",
+    day: "numeric",
+    month: "long", 
+    year: "numeric"
+};// options to pass to the toLocale method
+this.day = today.toLocaleString("us-En", options);
+}
