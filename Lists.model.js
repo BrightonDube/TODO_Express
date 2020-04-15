a49c6b16ff0eec5b4d4f0c6abfe3181a27a9fcdb
BrightonDube <BrightonDube@users.noreply.github.com>
2020-04-15 11:28:20 +0200

    add more lists

---
+++ Lists.model.js
@@ -0,0 +1,27 @@
+//jshint esversion: 8
+const mongoose = require('mongoose');
+//Use mongoose to connect to the Database that you need to work with
+mongoose.connect('mongodb://localhost/todo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//This gives confirmation of whether we are connected or not
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log(`Connection established to the ${db.name} Database.`);
+});
+
+//Let's create the schema that maps to the Lists collection
+const Schema = mongoose.Schema;
+
+const listSchema = new Schema({
+    name: String,
+    listItems: []
+});
+// create model for our list documents
+const List = new mongoose.model('list', listSchema);
+
+module.exports = List;
--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const port = 4000;
 const date = require(__dirname + '/date.js');
 const Task = require(`${__dirname}/Tasks.model.js`);
+const List = require(`${__dirname}/Lists.model.js`);
+const day = date.date;
+
 // view engine setup
 app.set('views', (__dirname + '/views'));
 app.set('view engine', 'ejs');
@@ -15,38 +18,75 @@ app.use(express.urlencoded({
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', async (req, res) => {
-  const day = date.date;
+app.get('/', async (req, res) => { 
   
   await Task.find().
     exec((err, tasks) => {
       if (err) {
         console.log(`An error has occured: ${err}`);
       } else {
-      let items = tasks;
-        console.log(items);
+        let items = tasks;
+
         res.render('index', { title: day, day: day, items: items });
       }
     });
-  
+});
+
+app.get("/:customList", async (req, res) => {
+  let customUrl = req.params.customList;
+
+  if (customUrl === "favicon.ico") res.redirect("/");
+  else {
+    const listFromDb = await List.findOne({ name: customUrl });
+    if (listFromDb === null) {
+      let list = new List({
+        name: customUrl,
+        listItems: customArray
+      });
+      list.save();
+      res.redirect("/"+ customUrl);
+    } else {
+      res.render('index', { title: customUrl, day: day, items: listFromDb.listItems });
+    }
+  } 
+
 });
 
 app.post("/", async (req, res) => {
   const item = req.body.item;
+  const listTitle = req.body.list;
+  let customArray;
   // use the model created in the Tasks.model
-  let task = new Task({content:item});
+  let task = new Task({ content: item });
   //task.content = item;
   //Save the task to the database.
+  if (listTitle === day){
   await Task.insertMany([task], function (err) {
     if (err) throw err;
     console.log('Task saved successfully!');
   });
   res.redirect('/');
+}
+else{
+  customArray = [];
+  customArray.push(item);
+  List.findOneAndUpdate({name:listTitle}, 
+    {$set: {listItems: customArray}}, 
+    (err)=>{
+      if (err) {
+        console.log("There was an error\n"+err);
+      } else {
+        console.log("List updated successfully");
+        res.redirect("/"+listTitle);
+      }
+    });
+  
+}
 });
 
-app.all('/:id', async(req, res)=>{
+app.post('/:id', async (req, res) => {
   let _id = req.params.id;
-  Task.findByIdAndDelete(_id, function (err) {
+  await Task.findByIdAndDelete(_id, function (err) {
     if (err) throw err;
     console.log('Task deleted successfully!');
   });
